41c163a2eeb98e5febcb424397d6d63a288a0daf
Diego Valobra <diegovalobra@gmail.com>
2024-11-02 20:10:21 +0100

    typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Slider from './components/slider/Slider';
 
 const loremIpsumContent = [
   {
-    title: "Forke me on git",
+    title: "Fork me on git",
     url: "https://github.com/pirolab/react-fullwith-gallery",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
